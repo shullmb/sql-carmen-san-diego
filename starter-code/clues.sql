2b37a2c389ef514d3ac8f2fd4aa2c5921d7ef4c2
shullmb <shullmb@gmail.com>
2018-06-11 14:45:33 -0700

    she is in Santa Monica

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,18 +2,25 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
+select name, population from country where region='Southern Europe' order by population;
+-- Holy See (Vatican City State)
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
-
+ 
+select code from country where name='Holy See (Vatican City State)';
+-- code: VAT
+select * from countrylanguage where countrycode='VAT';
+-- Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
-
+select countrycode, percentage from countrylanguage where language='Italian' order by percentage;
+--SMR
+select name from country where code='SMR';
+-- San Marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -21,18 +28,25 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
+select name from city where countrycode='SMR';
+-- Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
-
+select name from city where name like 'Serra%'
+--Serra
+select name,countrycode from city where name='Serra'
+--BRA
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+select capital from country where name='Brazil'
+-- 211
+select name from city where id='211';
+-- Bras�lia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -41,6 +55,11 @@
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
+
+select name from city where population='91084';
+-- Santa Monica
+select name, countrycode from city where population='91084'
+-- Santa Monica | USA
 
 -- Our play date of late has been unusually fun –
 -- As an agent, I'll say, you've been a joy to outrun.
